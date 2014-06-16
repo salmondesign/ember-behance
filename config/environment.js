3ba2696fd93e47607afb1b657c2f25107ee875b0
David Blurton <david.blurton@gmail.com>
2014-06-17 00:02:55 +0100

    Use experimental query params feature

--- config/environment.js
+++ config/environment.js
@@ -7,8 +7,7 @@ module.exports = function(environment) {
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
+        'query-params-new': true
       }
     },
 
