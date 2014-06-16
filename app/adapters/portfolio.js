304b6a549b97950fff4a2557189c1ff1c9f28222
David Blurton <david.blurton@gmail.com>
2014-06-16 14:18:06 +0100

    Need to use JSONP

--- app/adapters/portfolio.js
+++ app/adapters/portfolio.js
@@ -5,7 +5,7 @@ import memoize from './memoizer';
 export default Ember.Object.extend({
   findAll: memoize( function() {
     var url = 'http://www.behance.net/v2/users/rachelmsalmon/projects?api_key=np1FB2eTl6xF3iJHz6ttXpC5xKvFXkOP';
-    return ajax(url).then(function(data) {
+    return ajax(url, { dataType: 'jsonp' }).then(function(data) {
       return data.projects;
     });
   })
--- app/adapters/project.js
+++ app/adapters/project.js
@@ -5,7 +5,7 @@ import memoize from './memoizer';
 export default Ember.Object.extend({
   find: memoize( function(id) {
     var url = 'http://www.behance.net/v2/projects/' + id + '?api_key=np1FB2eTl6xF3iJHz6ttXpC5xKvFXkOP';
-    return ajax(url)
+    return ajax(url, { dataType: 'jsonp' })
       .then(function(data) {
         return data.project;
       });
