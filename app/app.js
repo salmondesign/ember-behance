6e0b527b2a3917d75a9e0ebaa59408a039465dee
David Blurton <david.blurton@gmail.com>
2014-07-21 21:02:01 +0100

    Move static header and banner to html for faster loading

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@ import loadInitializers from 'ember/load-initializers';
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
+  rootElement: '#app-container',
   modulePrefix: 'ember-behance-portfolio', // TODO: loaded via config
   Resolver: Resolver
 });
