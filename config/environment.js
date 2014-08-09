624e8588e63ae11e7974b4d24eeeac6f926aa237
David Blurton <david.blurton@gmail.com>
2014-08-09 13:40:13 +0100

    Use push state if available

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     environment: environment,
     baseURL: '/',
-    locationType: 'hash',
+    locationType: 'auto',
     EmberENV: {
       FEATURES: {
         'query-params-new': true
